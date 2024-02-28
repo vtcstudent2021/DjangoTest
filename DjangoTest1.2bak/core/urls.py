600469b55796405b04576cc0abb58dd2fbda75b8
vtcstudent2021 <97867644+vtcstudent2021@users.noreply.github.com>
2024-02-28 14:08:47 +0000

    123456

--- DjangoTest1.2bak/core/urls.py
+++ DjangoTest1.2bak/core/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls', namespace='store')),
     path('basket/', include('basket.urls', namespace='basket')),
+    path("accounts/", include("django.contrib.auth.urls")),    
 ]
 
 if settings.DEBUG:
